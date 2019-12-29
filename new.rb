90e8098bedbc96f62772fb73b01cf3a41526e4e3
Ranjeet Singh Sahni <singhranjeet5200@gmail.com>
2019-12-29 16:09:46 +0000

    Automatically backed up by Learn

--- new.rb
+++ new.rb
@@ -3,3 +3,6 @@ puts "In 2016, the president was #{current_president}."
 
 current_president = "Stephen Colbert"
 puts "Now, it being the year 2017, the president is #{current_president}."
+
+sound = "squeak"
+sound.upcase
